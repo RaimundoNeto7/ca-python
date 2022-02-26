0a4eacd2a76d3aa78d9440e980ab29f5f38356b0
Raimundo Neto <raimundoneto.rn17@gmail.com>
2022-02-25 23:28:35 -0400

    refactor: treating exceptions in products routes (GET and POST)

--- src/infra/http/fastapi_server.py
+++ src/infra/http/fastapi_server.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
-from src.application.controllers.products_controller import get_products_controller
+from src.application.controllers.products_controller import (
+    add_product_controller,
+    get_products_controller,
+)
+from src.application.schemas.products import ProductsSchema
 
 app = FastAPI()
 
@@ -12,4 +16,19 @@ def read_root():
 
 @app.get("/products")
 def get_products():
-    return get_products_controller()
+    try:
+        return get_products_controller()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.__traceback__
+        )
+
+
+@app.post("/products")
+def post_products(product: ProductsSchema):
+    try:
+        return add_product_controller(**product.dict())
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.__traceback__
+        )
