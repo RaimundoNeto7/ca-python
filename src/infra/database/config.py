9a101aebb3fd04377bdfea886518733b7d481928
Raimundo Neto <raimundo.neto@quintoandar.com.br>
2022-05-23 12:27:28 -0400

    refactor: add contextmanager decorator on get_db() function

--- src/infra/database/config.py
+++ src/infra/database/config.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -22,7 +23,7 @@ Base = declarative_base()
 def init_db():
     Base.metadata.create_all(bind=engine)
 
-
+@contextmanager
 def get_db():
     db = SessionLocal()
     try:
