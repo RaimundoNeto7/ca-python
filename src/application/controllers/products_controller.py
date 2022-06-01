a757c87d27720c6f1b26c7ed0819d0096264a861
Raimundo Neto <44676140+RaimundoNeto7@users.noreply.github.com>
2022-05-31 22:44:28 -0400

    fix: instantiate the session and the database repository in fastapi-server (#7)

--- src/application/controllers/products_controller.py
+++ src/application/controllers/products_controller.py
@@ -1,22 +1,25 @@
 from src.domain.use_cases.add_product import AddProduct
 from src.domain.use_cases.get_products import GetProducts
-from src.infra.database.config import get_db
-from src.infra.repository.products_repository_postgres import ProductsRepositoryPostgres
 
 
-def get_products_controller():
-    get_produtcs_use_case = GetProducts(ProductsRepositoryPostgres(get_db().__next__()))
-    return get_produtcs_use_case.execute()
+class ProductsController:
+    def __init__(self, repository) -> None:
+        self.repository = repository
 
+    def get_products_controller(self):
+        get_produtcs_use_case = GetProducts(self.repository)
+        return get_produtcs_use_case.execute()
 
-def add_product_controller(name, description, brand, price, quantity, is_active):
-    add_product_use_case = AddProduct(
-        ProductsRepositoryPostgres(get_db().__next__()),
-        name,
-        description,
-        brand,
-        price,
-        quantity,
-        is_active,
-    )
-    return add_product_use_case.execute()
+    def add_product_controller(
+        self, name, description, brand, price, quantity, is_active
+    ):
+        add_product_use_case = AddProduct(
+            self.repository,
+            name,
+            description,
+            brand,
+            price,
+            quantity,
+            is_active,
+        )
+        return add_product_use_case.execute()
